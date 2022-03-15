492259a6d4c2dbacf2d09a54a7ae459e0525f0c8
Victor Villacis <victor.villacis@outlook.com>
2022-03-14 21:19:37 -0400

    add track.js

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin'; //eslint-disable-line
 import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin'; //eslint-disable-line
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export default {
   mode: 'production',
@@ -14,15 +15,37 @@ export default {
     filename: '[name].[chunkhash].js',
   },
   plugins: [
+    // Generate an external css file with a hash in the filename
+    new MiniCssExtractPlugin({
+      filename: '[name].[chunkhash].css',
+    }),
+
     // Generate an HTML5 file that includes references to bundled JS.
     new HtmlWebpackPlugin({
       template: './src/index.html',
+      // Properties defined here will be available in index.html
+      title: 'App Name',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+      //Call this in the HTML using htmlWebpackPlugin.options.varName
+      //Only used in production
+      trackJSToken: '2060289379544c50a7d8f0e9177b8401',
     }),
   ],
   module: {
     rules: [
       {test: /\.js$/, exclude: /node_modules/, use: ['babel-loader']},
-      {test: /\.css$/, use: ['style-loader', 'css-loader']},
+      {test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader']},
     ],
   },
 };
